6f52491a50055c1eca94d96e3d55bd19baf7f565
kubrauysal <uysskubra@gmail.com>
2023-03-29 01:54:19 +0300

    update intro

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,12 @@ import "./Intro.css";
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Instagram from'../../img/instagram.png'
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
 
 
 const Intro = () => {
@@ -25,7 +31,9 @@ const Intro = () => {
         </div>
       </div>
       <div className="i-right">
-        i am right side
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={boy} alt="" />
       </div>
     </div>
   );
