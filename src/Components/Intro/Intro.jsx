557e34283ab9e4fac0d9fe91c6e2d8ae3c98772b
kubrauysal <uysskubra@gmail.com>
2023-03-28 03:24:02 +0300

    First commit

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Intro.css";
+import Github from '../../img/github.png';
+import LinkedIn from '../../img/linkedin.png';
+import Instagram from'../../img/instagram.png'
+
 
 const Intro = () => {
   return (
@@ -14,6 +18,11 @@ const Intro = () => {
           </span>
         </div>
         <button className="button i-button">Hire Me!</button>
+        <div className="i-icons">
+            <img src={Github} alt="" />
+            <img src={LinkedIn} alt="" />
+            <img src={Instagram} alt="" />
+        </div>
       </div>
       <div className="i-right">
         i am right side
